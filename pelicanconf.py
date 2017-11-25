5ef2e646abc2fc0f9b85fb42644a4c9fc3d5d1b7
Matt Burdan <matt_burdan@hotmail.com>
2017-11-24 21:12:30 -0800

    remove post_stats

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,7 +49,7 @@ COPYRIGHT_YEAR = 2017
 DEFAULT_PAGINATION = 10
 
 PLUGIN_PATHS = ['./pelican-plugins']
-PLUGINS = ['sitemap', 'post_stats', 'i18n_subsites']
+PLUGINS = ['sitemap', 'i18n_subsites']
 
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 
